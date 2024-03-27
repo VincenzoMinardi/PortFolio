eb1872a412a2a39edb4a05b3223ffecf4a2594de
VincenzoMinardi <vincenzo.minardi.mv@gmail.com>
2024-03-27 17:50:46 +0100

    install, vue, bootstrap

---
+++ vue-project/src/App.vue
@@ -0,0 +1,19 @@
+<script>
+import Information from './components/Information.vue';
+import NavBar from './components/NavBar.vue';
+
+
+export default {
+  components: {
+    NavBar,
+    Information,
+  }
+}
+</script>
+
+<template>
+  <NavBar />
+  <Information />
+</template>
+
+<style scoped></style>
