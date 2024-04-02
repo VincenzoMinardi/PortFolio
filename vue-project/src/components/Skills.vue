ac355f3bce2197c5a028ee9262fb439936d7f7a3
VincenzoMinardi <vincenzo.minardi.mv@gmail.com>
2024-04-02 15:39:12 +0200

    create file skills and project

---
+++ vue-project/src/components/Skills.vue
@@ -0,0 +1,65 @@
+<script>
+</script>
+
+<template>
+    <!-- Le mie Skills -->
+    <div class="skills p-3">
+        <h1 class="text-center text-white fw-bold animated fadeIn">Skills</h1>
+        <div class="cont-card d-flex justify-content-center flex-wrap animated fadeInDown p-4">
+            <div class="card mb-3 ms-3" style="width: 18rem;">
+                <img src="../img/img_skills/html.jpg" class="card-img-top" alt="">
+            </div>
+
+            <div class="card mb-3 ms-3" style="width: 18rem;">
+                <img src="../img/img_skills/css.jpg" class="card-img-top" alt="">
+            </div>
+
+            <div class="card mb-3 ms-3" style="width: 18rem;">
+                <img src="../img/img_skills/bootstrap.jpg" class="card-img-top" alt="">
+            </div>
+
+            <div class="card mb-3 ms-3" style="width: 18rem;">
+                <img src="../img/img_skills/javascript.jpg" class="card-img-top" alt="">
+            </div>
+
+            <div class="card mb-3 ms-3" style="width: 18rem;">
+                <img src="../img/img_skills/vue.jpg" class="card-img-top" alt="">
+            </div>
+
+            <div class="card mb-3 ms-3" style="width: 18rem;">
+                <img src="../img/img_skills/php.jpg" class="card-img-top" alt="">
+            </div>
+
+            <div class="card mb-3 ms-3" style="width: 18rem;">
+                <img src="../img/img_skills/laravel.jpg" class="card-img-top" alt="">
+            </div>
+
+            <div class="card mb-3 ms-3" style="width: 18rem;">
+                <img src="../img/img_skills/mysql.jpg" class="card-img-top" alt="">
+            </div>
+
+            <div class="card mb-3 ms-3" style="width: 18rem;">
+                <img src="../img/img_skills/postman.jpg" class="card-img-top" alt="">
+            </div>
+
+            <div class="card mb-3 ms-3" style="width: 18rem;">
+                <img src="../img/img_skills/sass.jpg" class="card-img-top" alt="">
+            </div>
+        </div>
+    </div>
+
+
+</template>
+
+<style scoped>
+.card-img-top {
+    height: 200px;
+    object-fit: contain;
+}
+
+.skills {
+    background-image: url(../img/foto3.jpg);
+    background-repeat: no-repeat;
+    background-size: cover;
+}
+</style>
